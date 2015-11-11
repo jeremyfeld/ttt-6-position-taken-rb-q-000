6a006bde2c2d799bdfe48b71293661e6d8705699
jeremyfeld <jeremyfeld@gmail.com>
2015-11-11 16:30:27 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, position)
   if board[position] == " " || board[position] == "" || board[position] == nil
     false
-  else board[position] == "X" || board[position] == "O"
+  else
     true
   end
 end
